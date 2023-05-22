a5e684cc19add237d36d2ac0c66254b3ed1bc2f1
RcleydsonR <rafael.cleydson@gmail.com>
2023-05-21 21:14:28 -0300

    #3 Adicionando campo de credito na model do Reciclador
    Co-authored-by: Joao-Moura <jaummoura15@gmail.com>

--- reciclador/models.py
+++ reciclador/models.py
@@ -6,3 +6,4 @@ from usuario.models import Usuario
 class Reciclador(models.Model):
     usuario = models.OneToOneField(Usuario, on_delete=models.PROTECT)
     cpf = models.CharField(max_length=11, unique=True)
+    credito = models.DecimalField(default=0, decimal_places=2, max_digits=7)
