9738ac00e93e69264a6ddf49e9a6c8ce365abb62
RcleydsonR <rafael.cleydson@gmail.com>
2023-05-22 01:00:56 -0300

    #2 Adicionando campo de limite_maximo para configuração de notificação do Estabelecimento
    Co-authored-by: Joao-Moura <jaummoura15@gmail.com>

--- estabelecimento/models.py
+++ estabelecimento/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 from usuario.models import Usuario
 
@@ -6,3 +7,7 @@ from usuario.models import Usuario
 class Estabelecimento(models.Model):
     usuario = models.OneToOneField(Usuario, on_delete=models.PROTECT)
     cnpj = models.CharField(max_length=14, unique=True)
+    limite_maximo = models.DecimalField(
+        decimal_places=2, max_digits=4, default=70,
+        validators=[MinValueValidator(10), MaxValueValidator(100)]
+    )
