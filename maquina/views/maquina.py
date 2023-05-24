564bb6680573879359ada5aa259bfba2f504d6c4
lbvx <lucasbraunxav@gmail.com>
2023-05-24 15:02:07 -0300

    List Maquina filtra por usuario
    Co-Authored-by: GRVial <guivial2010@gmail.com>

--- maquina/views/maquina.py
+++ maquina/views/maquina.py
@@ -2,11 +2,19 @@ from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateAP
 
 from maquina.serializers.maquina import MaquinaSerializer, MaquinaEsvaziarSerializer
 from maquina.models import Maquina
-
+from estabelecimento.models import Estabelecimento
 
 class MaquinaListView(ListAPIView):
-    queryset = Maquina.objects.all()
     serializer_class = MaquinaSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if Estabelecimento.objects.filter(usuario=user).exists():
+            est = Estabelecimento.objects.get(usuario=user)
+            queryset = Maquina.objects.filter(estabelecimento=est)
+        else:
+            queryset = Maquina.objects.exclude(estabelecimento=None)
+        return queryset
 
 
 class MaquinaUpdateRetrieveView(RetrieveUpdateAPIView):
