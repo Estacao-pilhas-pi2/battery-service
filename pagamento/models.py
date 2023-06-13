a8b5aba9277806ac01ef979bbd851fe0e40a8b48
RcleydsonR <rafael.cleydson@gmail.com>
2023-06-12 22:52:39 -0300

    Adicionando redundância de preços das pilhas na model de pagamento e reciclador para prover histórico
    Co-authored-by: Joao-Moura <jaummoura15@gmail.com>

--- pagamento/models.py
+++ pagamento/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 from maquina.models import Maquina
 
+from reciclador.models import Reciclador
+
 
 class Pagamento(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -12,15 +14,22 @@ class Pagamento(models.Model):
     quantidade_pilha_C = models.IntegerField(default=0)
     quantidade_pilha_D = models.IntegerField(default=0)
     quantidade_pilha_V9 = models.IntegerField(default=0)
+    preco_AAA = models.DecimalField(default=0, decimal_places=2, max_digits=7)
+    preco_AA = models.DecimalField(default=0, decimal_places=2, max_digits=7)
+    preco_C = models.DecimalField(default=0, decimal_places=2, max_digits=7)
+    preco_D = models.DecimalField(default=0, decimal_places=2, max_digits=7)
+    preco_V9 = models.DecimalField(default=0, decimal_places=2, max_digits=7)
 
     utilizado = models.BooleanField(default=False)
     data_vencimento = models.DateTimeField()
+
+    reciclador = models.ForeignKey(Reciclador, on_delete=models.PROTECT, null=True)
 
     maquina = models.ForeignKey(Maquina, on_delete=models.CASCADE)
 
     @property
     def valor_total(self):
         return sum([
-            getattr(self, f'quantidade_pilha_{tipo}') * getattr(self.maquina, f'preco_{tipo}')
+            getattr(self, f'quantidade_pilha_{tipo}') * getattr(self, f'preco_{tipo}')
             for tipo in ['AAA', 'AA', 'C', 'D', 'V9']
         ])
